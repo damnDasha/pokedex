481542b7bcce4a27024ee0e3d66e79ddb28798ca
dasha akhten <dasha.akhten@gmail.com>
2021-07-21 20:57:46 -0700

    feat: some components and a functioning button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { AddPokemonComponent } from './component/add-pokemon/add-pokemon.component';
+import { ButtonComponent } from './component/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddPokemonComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
